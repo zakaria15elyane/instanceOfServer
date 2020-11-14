9d14141c2240b6bdbed8af1da4e667dec3e9274c
zakaria15elyane <zakaria15elyane@gmail.com>
2020-11-14 21:55:17 +0100

    first

---
+++ src/main/java/com/coding/HdfcStockApplication.java
@@ -0,0 +1,13 @@
+package com.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfcStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdfcStockApplication.class, args);
+	}
+
+}
